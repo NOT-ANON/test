186b927509367ec4d7946711a02eeb18564107d8
NOT-ANON <80609067+NOT-ANON@users.noreply.github.com>
2022-05-29 11:52:19 -0700

    Update file2.lua

--- file2.lua
+++ file2.lua
@@ -1,1 +1,17 @@
+local pv = game.PlaceVersion
 
+function notify(message,color,time)
+    args = {
+        [1] = message,
+        [2] = color,
+        [3] = time
+    }
+    firesignal(game.ReplicatedStorage.Events.SystemMessage.OnClientEvent, unpack(args))
+end
+
+if pv == 263 then
+    notify("OPPOSER VR Hax Loaded", "blue", 10)
+    loadstring()
+else
+    notify("OPPOSER VR HAS UPDATED WAIT FOR FIX", "red", 10)
+end
